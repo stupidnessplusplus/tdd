04dc69ceba60571a76e3576f7eb6b3472b23102a
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-11-17 17:17:38 +0500

    Add SizeParser.cs

---
+++ homework/TagsCloudVisualization/SizeParser.cs
@@ -0,0 +1,24 @@
+using System.Drawing;
+
+namespace TagsCloudVisualization;
+
+public class SizeParser
+{
+    public bool TryParse(
+        string str,
+        out Size size)
+    {
+        var lineParts = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        if (lineParts.Length == 2
+            && int.TryParse(lineParts[0], out var width)
+            && int.TryParse(lineParts[1], out var height))
+        {
+            size = new Size(width, height);
+            return true;
+        }
+
+        size = default;
+        return false;
+    }
+}
