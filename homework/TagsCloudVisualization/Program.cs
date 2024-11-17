9f9d08e447539ad7edc9ec630319caceeaa7febc
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-11-17 17:17:56 +0500

    Add Program.cs with Main method

---
+++ homework/TagsCloudVisualization/Program.cs
@@ -0,0 +1,71 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+using TagsCloud;
+
+namespace TagsCloudVisualization;
+
+public static class Program
+{
+    private const string StandardInputPath = "in.txt";
+    private const string StandardOutputPath = "out.png";
+
+    public static void Main()
+    {
+        try
+        {
+            Run(StandardInputPath, StandardOutputPath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static void Run(
+        string inputPath,
+        string outputPath)
+    {
+        var layouter = new SpiralCircularCloudLayouter(Point.Empty);
+        var visualizer = new RectanglesVisualizer(1_024, 1_024);
+        var rectangles = PutRectanglesFromFile(layouter, inputPath);
+
+        visualizer.AddRectangles(rectangles);
+        visualizer.Image.Save(outputPath, ImageFormat.Png);
+    }
+
+    private static IEnumerable<Rectangle> PutRectanglesFromFile(
+        ICircularCloudLayouter layouter,
+        string path)
+    {
+        var sizeParser = new SizeParser();
+        var lines = ReadLinesFromFile(path);
+
+        foreach (var line in lines)
+        {
+            if (sizeParser.TryParse(line, out var size))
+            {
+                yield return layouter.PutNextRectangle(size);
+            }
+            else
+            {
+                Console.WriteLine($"Unable to parse line '{line}'");
+            }
+        }
+    }
+
+    private static IEnumerable<string> ReadLinesFromFile(
+        string path)
+    {
+        using var reader = new StreamReader(path);
+
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                yield return line;
+            }
+        }
+    }
+}
